c9c2f3b0b846099a45b7db2782986963545d7115
rvirji <rvirji@vendasta.com>
2018-07-25 16:02:09 -0600

    Use assets version of material examples

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,7 +5,7 @@ import {LocationStrategy, PathLocationStrategy} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {MatNativeDateModule} from '@angular/material';
-import {ExampleModule} from '@angular/material-examples';
+import {ExampleModule} from '../assets/stackblitz/examples';
 
 import {MaterialDocsApp} from './material-docs-app';
 import {HomepageModule} from './pages/homepage/homepage';

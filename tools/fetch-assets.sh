33a45a050b324b1ee82462d822e9d3abb954366e
rvirji <rvirji@vendasta.com>
2018-09-05 14:30:48 -0600

    Instead of getting docs from github, get it from the gcs tar

--- tools/fetch-assets.sh
+++ tools/fetch-assets.sh
@@ -18,11 +18,8 @@ stackblitzDestination=./src/assets/stackblitz/
 # Path to the @angular/material-examples package in the node modules.
 materialExamplesDestination=./node_modules/@angular/material-examples
 
-# Git HTTP clone URL for the material2-docs-content repository.
-docsContentRepoUrl="https://github.com/angular/material2-docs-content"
-
 # Path to the directory where the docs-content will be cloned.
-docsContentPath=/tmp/material2-docs-content
+docsContentPath=./tmp/material2-docs-content
 
 # Create all necessary directories if they don't exist.
 mkdir -p ${documentsDestination}
@@ -33,21 +30,22 @@ mkdir -p ${materialExamplesDestination}
 # Remove previous repository if directory is present.
 rm -Rf ${docsContentPath}
 
-# Clone the docs-content repository.
-git clone ${docsContentRepoUrl} ${docsContentPath}
-
-# Set commit to the installed version of angular material
-materialVersion=$(npm list @angular/material | tail -n 2 | head -n 1 | sed 's#.*/material@\(.*\)#\1#' | tr -d '[:space:]')
-cd ${docsContentPath}
-commitHash=$(git log | grep "master" -B 4 | grep "changelog" -B 4 | grep ${materialVersion} -B 4 | grep "commit" | awk '{print $2}' | head -n 1)
-echo "Getting docs content from angular/material2-docs-content"
-echo "@angular/material version: ${materialVersion}, docs commit: ${commitHash}"
-git reset --hard ${commitHash}
-cd -
+mkdir tmp
+cd tmp
+git remote add material2 https://github.com/vendasta/material2
+git fetch --all
+latestCommit=$(git log material2/master | head -n 1 | awk '{print $2}')
+materialDocsFileUrl=$(curl -X GET "https://www.googleapis.com/storage/v1/b/vendasta-material-docs/o" | grep `git log material2/master | head -n 1 | awk '{print $2}'` | grep mediaLink | awk '{print $2}' | cut -d '"' -f 2)
+curl -X GET -o ./material-docs.tar.gz ${materialDocsFileUrl}
+tar -xzvf material-docs.tar.gz
+rm -rf material-docs.tar.gz
+cd material2-docs-content
+docsContentPath=$(pwd)
+cd ../..
 
 # Copy all document assets (API, overview and guides).
-cp -R ${docsContentPath}/api ${documentsDestination}
-cp -R ${docsContentPath}/overview ${documentsDestination}
+cp -R ${docsContentPath}/api/* ${documentsDestination}
+cp -R ${docsContentPath}/overview/ ${documentsDestination}
 cp -R ${docsContentPath}/guides ${documentsDestination}
 
 # Copy the example assets to the docs.
@@ -56,5 +54,5 @@ cp -R ${docsContentPath}/examples ${examplesDestination}
 # Copy the StackBlitz examples to the docs.
 cp -R ${docsContentPath}/stackblitz/examples ${stackblitzDestination}
 
-# Manually install the @angular/material-examples package from the docs-content.
+# Manually install the @angular/material-examples p ackage from the docs-content.
 cp -R ${docsContentPath}/examples-package/* ${materialExamplesDestination}
